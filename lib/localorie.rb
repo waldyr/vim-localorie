a35a4ace8f66a0b570ede9e0bc684bc9aa543ee0
Waldyr de Souza <waldyr.ar@gmail.com>
2018-07-12 11:46:54 +0200

    Remove non-en translation files

--- lib/localorie.rb
+++ lib/localorie.rb
@@ -81,7 +81,6 @@ end
 #
 #####
 
-
 def add_source_file_yaml!(hash, file)
   hash.each do |_, v|
     if v.is_a? Hash
@@ -94,35 +93,12 @@ def add_source_file_yaml!(hash, file)
   end
 end
 
-# def add_source_file_rb!(hash, file)
-#   hash.each do |k, v|
-#     if v.is_a? Hash
-#       add_source_file_rb! v, file
-#     else
-#       hash[k] = {'file' => file, 'value' => v}
-#     end
-#   end
-# end
-
-
 translations_yml = {}
-Dir.glob("#{ARGV[0]}/config/locales/**/*.yml").each do |f|
+
+Dir.glob("#{ARGV[0]}/config/locales/**/*en.yml").each do |f|
   hash = parse File.read(f)
   add_source_file_yaml! hash, f
   translations_yml = translations_yml.deep_merge hash
 end
 
-# TODO: get line numbers of i18n keys in ruby locale files.
-translations_rb = {}
-# Dir.glob("#{ARGV[0]}/config/locales/**/*.rb").each do |f|
-#   # The i18n gem loads ruby locale files this way:
-#   # https://github.com/svenfuchs/i18n/blob/ea63d740b61fa55abb6799a5decbf9ee8fdbec35/lib/i18n/backend/base.rb#L175
-#   hash = eval IO.read(f), binding, f
-#   hash.stringify_keys!
-#   add_source_file_rb! hash, f
-#   translations_rb = translations_rb.deep_merge hash
-# end
-
-translations = translations_yml.deep_merge translations_rb
-puts translations.to_json
-
+puts translations_yml.to_json
